6b6f0e512224bdda26e3e0f0c9499dac82f98978
Gonnuru <sampath.gonnuru@gmail.com>
2018-10-25 22:21:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/uber/HospitalsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.uber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitals_list);
+    }
+}
